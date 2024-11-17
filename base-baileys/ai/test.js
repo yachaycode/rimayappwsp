91351693fdee93118539dfb0805e419986203d08
Alejandro Hurtado <alejandro.hurtado@scanntech.com>
2024-11-17 01:52:46 -0500

    modify refer ai app rimay

--- base-baileys/ai/test.js
+++
@@ -1,7 +0,0 @@
-const { askChatGpt } = require('./chatgpt');
-
-(async () => {
-    const question = "¿Qué hora es en brisbane Australia?";
-    const answer = await askChatGpt(question);
-    console.log('Respuesta de ChatGPT:', answer);
-})();
